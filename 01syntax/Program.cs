0c08e6afe9013ee36c29ff6ed673622cbda3c347
Tomáš Kazda <tomas.kazda@pslib.cz>
2019-09-13 11:06:21 +0200

    lesson2

---
+++ 01syntax/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _01syntax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int start, count, step;
+
+            
+            for (int _a = 1; 3 > _a; _a += 1 )
+            {
+                Console.WriteLine("JES");
+            }
+
+            Console.WriteLine("DAN");
+                
+            Console.WriteLine("Hello World!");
+
+            Console.ReadKey();
+        }
+    }
+}
