46f781745cf76b58131d9138d3740dc16303bf9a
Tomáš Kazda <tomas.kazda@pslib.cz>
2019-10-18 08:02:11 +0200

    ArrayUnlimited

---
+++ 03ArrayUnlimited/IDynamicArray.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArrayUnlimited
+{
+    interface IDynamicArray
+    {
+        object Get(int position);
+        void Insert(object value, int position);
+        void ShiftItems(int idexFrom);
+        int Count { get; }
+        int Length { get; }
+    }
+}
---
+++ 03ArrayUnlimited/UnlimitedArray.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArrayUnlimited
+{
+    class UnlimitedArray: IDynamicArray
+    {
+        /// <summary>
+        ///   Zvětší nebo zmenší pole prvků
+        /// </summary>
+        /// <param name="oldArray">pole, které bude zvětšeno nebo zmenšeno</param>
+        /// <param name="newSize">velikost pole po zvětšení / zmenšení</param>
+        private static void ResizeArray(ref object[] oldArray, int newSize)
+        {
+
+        }
+    }
+}
